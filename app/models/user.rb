b88cbef3a59f12a1722620558c7fad6f5c2b6408
dveldman01 <dveldman@chicagobooth.edu>
2020-12-01 22:23:29 +0000

    Generated resources, added associations and validations

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+
+has_many(:photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+has_many(:received_follow_requests, { :class_name => "Followrequest", :foreign_key => "recipient_id", :dependent => :destroy })
+has_many(:sent_follow_requests, { :class_name => "Followrequest", :foreign_key => "sender_id", :dependent => :destroy })
+has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+
+has_many(:recipients, { :through => :sent_follow_requests, :source => :recipient })
+has_many(:received_follow_requests, { :through => :received_follow_requests, :source => :sender })
+
+validates(:username, { :presence => true })
+validates(:username, { :uniqueness => true })
+validates(:private, { :presence => true })
+
+
+end
